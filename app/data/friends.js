a734ebf8ff3c230ed6cecc15c3e2c34fa37f7aa0
mahmoud zougheb <mahmoudzougheb@mahmouds-MBP.attlocal.net>
2017-10-20 23:38:47 -0700

    pushing the last editio

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+   {
+      "name":"Mazen",
+      "photo":"https://pbs.twimg.com/profile_images/834430522600329216/2jY2gB8a.jpg",
+      "scores":[5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+      "name":"Ibraheem",
+      "photo":"http://im.rediff.com/sports/2016/mar/28ibra.jpg",
+      "scores":[4,2,5,1,3,2,2,1,3,2]
+   },
+   {
+      "name":"Essam",
+      "photo":"http://www1.pictures.zimbio.com/gi/Hassan+Shehata+Egypt+v+Italy+FIFA+Confederations+5Cax9l2VgT4l.jpg",
+      "scores":[5,2,2,2,4,1,3,2,5,5]
+   },
+   {
+      "name":"Refaat",
+      "photo":"http://www.cairostadium.com/wp-content/uploads/2017/07/631996_0-674x431.jpg",
+      "scores":[3,3,4,2,2,1,3,2,2,3]
+   },
+	{
+	   "name":"Badway",
+	   "photo":"http://ichef.bbci.co.uk/onesport/cps/480/cpsprodpb/0614/production/_93265510_elhadary.jpg",
+	   "scores":[4,3,4,1,5,2,5,3,1,4]
+	},
+      {
+      "name":"Dahsh",
+      "photo":"https://tmssl.akamaized.net//images/portrait/originals/148455-1417606422.jpg",
+      "scores":[3,3,4,1,4,3,5,3,5,4]
+   },
+	{
+	   "name":"Khaled",
+	   "photo":"http://cdn3-i.hitc-s.com/498/stoke_citys_ramadan_sobhi_celebrates_their_second_goal_371603.jpg",
+	   "scores":[4,4,2,3,2,2,3,2,4,5]
+	}
+
+]
+
+module.exports = friends;
